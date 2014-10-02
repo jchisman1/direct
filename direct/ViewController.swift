84fd313d6f4e7ea1e3c8abecc3f360431d0093a3
jchisman1 <jchisman1@yahoo.com>
2014-10-01 23:55:30 -0400

    Initial Commit

---
+++ direct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  direct
+//
+//  Created by James Chisolm on 10/1/14.
+//  Copyright (c) 2014 James Chisolm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
